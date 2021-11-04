0cd9d6a2c7af2ab3d3c432b62433ca7c8878c4da
Alexis Lemaire <alexis.lemaire2021@campus-eni.fr>
2021-11-04 18:16:47 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend-angular';
+export class AppComponent implements OnInit {
+  pseudo? = localStorage.getItem('pseudo');
+  messages?: any[];
+  form!: FormGroup;
+  form2!: FormGroup;
+  message?: { author: string, message: string };
+
+  constructor(private fb: FormBuilder, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    console.log('pseudo', this.pseudo);
+    this.getMessages();
+
+    this.form = this.fb.group({
+      pseudo: ['', Validators.required]
+    })
+
+    this.form2 = this.fb.group({
+      message: ['', Validators.required]
+    })
+  }
+
+  async setPseudo() {
+    await localStorage.setItem('pseudo', this.form.value.pseudo);
+    location.reload();
+  }
+
+  sendMessage() {
+    this.message = { author: this.pseudo || "", message: this.form2.value.message };
+    this.http.post("http://nest-js-chat.herokuapp.com/messages", this.message).subscribe( res => console.log(res));
+    location.reload();
+  }
+
+  getMessages() {
+    this.http.get("http://nest-js-chat.herokuapp.com/messages").subscribe( (res : any) => this.messages = res);
+  }
 }
