042a8869b96127fed2d5b1580e7b9b57f3a20407
Alexis Lemaire <alexis.lemaire2021@campus-eni.fr>
2021-11-07 19:00:21 +0100

    feat: add Deleting One Message Functionnality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,9 +17,11 @@ export class AppComponent implements OnInit {
   constructor(private fb: FormBuilder, private http: HttpClient) {}
 
   ngOnInit(): void {
-    console.log('pseudo', this.pseudo);
     this.getMessages();
+    this.setForms();
+  }
 
+  setForms(){
     this.form = this.fb.group({
       pseudo: ['', Validators.required]
     })
@@ -34,13 +36,23 @@ export class AppComponent implements OnInit {
     location.reload();
   }
 
-  sendMessage() {
-    this.message = { author: this.pseudo || "", message: this.form2.value.message };
-    this.http.post("https://nest-js-chat-api.herokuapp.com/messages", this.message).subscribe( res => console.log(res));
+  async sendMessage() {
+    await (this.message = { author: this.pseudo || "", message: this.form2.value.message });
+    await this.http.post("https://nest-js-chat-api.herokuapp.com/messages", this.message).subscribe( res => console.log(res), err => console.log(err));
     location.reload();
   }
 
   getMessages() {
     this.http.get("https://nest-js-chat-api.herokuapp.com/messages").subscribe( (res : any) => this.messages = res);
+  }
+
+  async deleteMessage(id: number) {
+    await this.http.delete("https://nest-js-chat-api.herokuapp.com/messages/"+id).subscribe( res => console.log(res), err => console.log(err));
+    location.reload();
+  }
+
+  async logout(){
+    await localStorage.clear();
+    location.reload();
   }
 }
